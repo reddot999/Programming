a5d1c5af051d52bee2c3152ef2ea993681f66dd7
reddot999 <cipriani_mattia@tuta.com>
2025-06-06 15:38:44 +0200

    prove di cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-640b4587ee43779d.d
@@ -0,0 +1,5 @@
+/home/mattia/Programming/hello_cargo/target/debug/deps/libhello_cargo-640b4587ee43779d.rmeta: src/main.rs
+
+/home/mattia/Programming/hello_cargo/target/debug/deps/hello_cargo-640b4587ee43779d.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-847d4af85fc7ff08.d
@@ -0,0 +1,5 @@
+/home/mattia/Programming/hello_cargo/target/debug/deps/libhello_cargo-847d4af85fc7ff08.rmeta: src/main.rs
+
+/home/mattia/Programming/hello_cargo/target/debug/deps/hello_cargo-847d4af85fc7ff08.d: src/main.rs
+
+src/main.rs:
