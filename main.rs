6499c2a474aa6a95e8c58bcadb1ad66b7d63e6df
reddot999 <cipriani_mattia@tuta.com>
2025-06-06 15:32:42 +0200

    prove di studio rust

---
+++ main.rs
@@ -0,0 +1,6 @@
+fn main () {
+    println!("Hello, world!");
+    // This is a simple Rust program that prints "Hello, world!" to the console.
+    // You can run this code using `cargo run` in your terminal.
+    // Make sure you have Rust installed on your system.
+}
