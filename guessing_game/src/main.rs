a095b7b936a8a1d96d83bfb9f1d4f6c824898168
reddot999 <cipriani_mattia@tuta.com>
2025-06-06 17:17:52 +0200

    aggiunto un loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,24 +5,33 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-
-    let secret_number: u32 = rand::rng().random_range(1..=100);
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
     
-    println!("You guessed: {guess}");
+    loop {
+        println!("Please input your guess (1-100):");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!, the secret number was {secret_number}"),
-        Ordering::Greater => println!("Too big!, the secret number was {secret_number}"),
-        Ordering::Equal => println!("You win!, the secret number was {secret_number}"),
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a valid number!");
+                continue;
+            }
+        };
+
+        let secret_number: u32 = rand::rng().random_range(1..=100);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!, the secret number was {secret_number}"),
+            Ordering::Greater => println!("Too big!, the secret number was {secret_number}"),
+            Ordering::Equal => {
+                println!("You win!, the secret number was {secret_number}");
+                break;
+            }
+        }
     }
-    println!("Thank you for playing!");
 }
