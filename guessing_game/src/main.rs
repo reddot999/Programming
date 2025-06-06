30a1abab5b645a523d0594b81534ca1a6561037e
reddot999 <cipriani_mattia@tuta.com>
2025-06-06 17:03:04 +0200

    sistemato guessing_game/src/main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,10 +6,7 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number:u32  = rand::rng().random_range(1..=100);
-
-    //println!("The secret number is: {secret_number}");
-
+    let secret_number: u32 = rand::rng().random_range(1..=100);
     println!("Please input your guess.");
 
     let mut guess = String::new();
